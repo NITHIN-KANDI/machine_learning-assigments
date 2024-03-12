2f47850379d6e91ee6ca248e91981f8b08771473
NITHIN_KANDI <116893619+NITHIN-KANDI@users.noreply.github.com>
2024-03-12 11:27:07 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,13 +1,13 @@
-def commondigits(in_list1, in_list2):
+def common_digits(input_list1, input_list2):
     # Convert input lists to sets to remove duplicate elements and enable set operations
-    list1 = set(in_list1)
-    list2 = set(in_list2)
+    set1 = set(input_list1)
+    set2 = set(input_list2)
     
     # Find the common elements between the sets
-    common_digits = list1.intersection(list2)
+    common_digits_set = set1.intersection(set2)
     
     # Return the count of common elements
-    return len(common_digits)
+    return len(common_digits_set)
 
 # Example lists
 list1 = [5, 4, 3, 6, 7]
@@ -17,5 +17,5 @@ print("The first list is:", list1)
 print("The second list is:", list2)
 
 # Call the function and print the result
-common_digit_count = commondigits(list1, list2)
-print("The count of common elements:", common_digit_count)
+common_digit_count = common_digits(list1, list2)
+print("The count of common elements between the lists is:", common_digit_count)
